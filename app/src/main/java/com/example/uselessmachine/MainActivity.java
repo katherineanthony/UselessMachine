a375be032e10cf7e736a9d2fd768316082e1ba26
katherineanthony <katherinesanthony@gmail.com>
2019-08-28 13:56:47 -0700

    started project

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.uselessmachine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switchy;
+    private Button buttonSD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wireWidgets();
+        setListeners();
+    }
+
+    private void wireWidgets()
+    {
+        switchy = findViewById(R.id.switch_main_useless);
+        buttonSD = findViewById(R.id.button_main_selfDestruct);
+    }
+
+    private void setListeners()
+    {
+        switchy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, final boolean isChecked) {
+
+                if(isChecked) {
+                    new CountDownTimer(2000, 10) {
+                        @Override
+                        public void onTick(long l) {
+                            if(!switchy.isChecked())
+                            {
+                                cancel();
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            switchy.setChecked(false);
+                        }
+                    }.start();
+                }
+
+//                if(isChecked){
+////                    Toast.makeText(MainActivity.this, "toasty", Toast.LENGTH_SHORT).show();
+////                }
+////                    else{
+////                    Toast.makeText(MainActivity.this, "not toasty", Toast.LENGTH_SHORT).show();
+////                    }
+            }
+        });
+
     }
 }
