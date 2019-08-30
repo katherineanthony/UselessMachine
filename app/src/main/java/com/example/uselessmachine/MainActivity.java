10ba20bbec7a39c5960ce3f112bd6d6734ca7961
katherineanthony <katherinesanthony@gmail.com>
2019-08-30 13:52:50 -0700

    almost done!

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.uselessmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Switch switchy;
     private Button buttonSD;
+    private ConstraintLayout constraintLayout;
+    //private int time;
+    //private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +33,51 @@ public class MainActivity extends AppCompatActivity {
     {
         switchy = findViewById(R.id.switch_main_useless);
         buttonSD = findViewById(R.id.button_main_selfDestruct);
+        constraintLayout = findViewById(R.id.constraintLayout_main_layout);
     }
 
     private void setListeners()
     {
         switchy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, final boolean isChecked) {
+                                               @Override
+                                               public void onCheckedChanged(CompoundButton compoundButton, final boolean isChecked) {
 
-                if(isChecked) {
-                    new CountDownTimer(2000, 10) {
+                                                   if (isChecked) {
+                                                       new CountDownTimer(2000, 10) {
+                                                           @Override
+                                                           public void onTick(long l) {
+                                                               if (!switchy.isChecked()) {
+                                                                   cancel();
+                                                               }
+                                                           }
+
+                                                           @Override
+                                                           public void onFinish() {
+                                                               switchy.setChecked(false);
+                                                           }
+                                                       }.start();
+                                                   }
+                                               }
+                                           });
+        buttonSD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                    new CountDownTimer(10000, 1000) {
+                        int x = 10;
+
                         @Override
                         public void onTick(long l) {
-                            if(!switchy.isChecked())
-                            {
-                                cancel();
-                            }
+                            x--;
+                            buttonSD.setText(String.valueOf(x));
                         }
 
                         @Override
                         public void onFinish() {
-                            switchy.setChecked(false);
+                            finish();
                         }
                     }.start();
-                }
 
-//                if(isChecked){
-////                    Toast.makeText(MainActivity.this, "toasty", Toast.LENGTH_SHORT).show();
-////                }
-////                    else{
-////                    Toast.makeText(MainActivity.this, "not toasty", Toast.LENGTH_SHORT).show();
-////                    }
-            }
+                }
         });
 
     }
